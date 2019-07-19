e803c1ec535986c2eaa9be6f2399c65c6bae4162
Lucas Sosa <sosalucas87@gmail.com>
2019-07-19 13:38:53 -0300

    Setting start configuration

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+/* 
+ * Copyright (C) PowerOn Sistemas - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Lucas Sosa <sosalucas87@gmail.com>, Octubre 2018
+ */
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
@@ -14,20 +20,15 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . '/../src/settings/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-$dependencies = require __DIR__ . '/../src/dependencies.php';
-$dependencies($app);
-
-// Register middleware
-$middleware = require __DIR__ . '/../src/middleware.php';
-$middleware($app);
+require __DIR__ . '/../src/dependencies/database.php';
+require __DIR__ . '/../src/dependencies/monolog.php';
 
 // Register routes
-$routes = require __DIR__ . '/../src/routes.php';
-$routes($app);
+require __DIR__ . '/../src/routes/home.php';
 
 // Run app
 $app->run();
